514b4a5bf51f93543f5b3c675619f437da58a41a
terrakuh <greybley@yahoo.de>
2018-09-29 11:56:51 +0200

    Change sqlite3_connection to handle

---
+++ sqlite3/handle.cpp
@@ -0,0 +1,26 @@
+#include "handle.hpp"
+
+#include <utility>
+
+
+namespace sqlite3
+{
+
+handle::handle(std::function<void(void*)> && _destructor) noexcept : _destructor(std::move(_destructor))
+{
+	_handle = nullptr;
+}
+
+handle::handle(const std::function<void(void*)> & _destructor) noexcept : _destructor(_destructor)
+{
+	_handle = nullptr;
+}
+
+handle::~handle()
+{
+	if (_handle) {
+		_destructor(_handle);
+	}
+}
+
+}
---
+++ sqlite3/handle.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <functional>
+
+
+namespace sqlite3
+{
+
+class handle
+{
+public:
+	handle(std::function<void(void*)> && _destructor) noexcept;
+	handle(const std::function<void(void*)> & _destructor) noexcept;
+	~handle();
+	template<typename Type>
+	Type *& get()
+	{
+		return reinterpret_cast<Type*>(_handle);
+	}
+
+private:
+	void * _handle;
+	std::function<void(void*)> _destructor;
+};
+
+}

06831041193fed13b0a89439fdc79ee558590945
terrakuh <greybley@yahoo.de>
2018-10-02 20:31:22 +0200

    Remove overhead_per_encryption()

--- sqlite3/encryption_context.hpp
+++ sqlite3/encryption_context.hpp
@@ -14,7 +14,6 @@ class encryption_context
 public:
 	virtual void encrypt(int64_t _id, const void * _buffer, size_t _size, void * _output) = 0;
 	virtual void decrypt(int64_t _id, const void * _buffer, size_t _size, void * _output, std::function<int(void*, int, int64_t)> _read_callback) = 0;
-	virtual size_t overhead_per_encryption() const = 0;
 	virtual std::vector<std::tuple<const void*, size_t, int64_t>> modified_overhead_sectors() = 0;
 };
 

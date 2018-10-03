16f2c5c2259ba6455a9cee25b3309096e3d5ab0b
terrakuh <greybley@yahoo.de>
2018-10-03 19:17:27 +0200

    Update encryption

--- sqlite3/encrypted_vfs.cpp
+++ sqlite3/encrypted_vfs.cpp
@@ -9,6 +9,8 @@
 #include <sstream>
 #include <iomanip>
 
+#define PRINT_OUTPUT
+
 
 namespace sqlite3
 {
@@ -22,13 +24,19 @@ sqlite3_vfs encrypted_vfs;
 
 // The base function of the default VFS
 xopen_signature xopen_base = nullptr;
+constexpr auto sqlite_header_size = 100;
+constexpr auto header_control_byte = -1;
+constexpr auto header_read = 0x1;
+constexpr auto header_modified = 0x2;
 
 
 inline int xclose_derived(sqlite3_file * _file)
 {
+	auto _header = *reinterpret_cast<int8_t**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 3) - 1;
 	auto _base_methods = *reinterpret_cast<base_method_type*>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*));
 
 	// Delete custom method table
+	delete[] _header;
 	delete _file->pMethods;
 	_file->pMethods = _base_methods;
 
@@ -44,26 +52,36 @@ inline int xread_base(sqlite3_file * _file, void * _buffer, int _size, sqlite3_i
 
 inline int xread_derived(sqlite3_file * _file, void * _buffer, int _size, sqlite3_int64 _offset)
 {
+#if defined(PRINT_OUTPUT)
+	printf("[%p]: read %i bytes from %lli\n", _file, _size, _offset);
+#endif
+
+	auto _header = *reinterpret_cast<int8_t**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 3);
+
+	// Read header
+	if (!(_header[header_control_byte] & header_read)) {
+		auto _result = xread_base(_file, _header, sqlite_header_size, 0);
+
+		if (_result != SQLITE_OK) {
+			return _result;
+		}
+
+		_header[header_control_byte] |= header_read;
+	}
+
+	// Read from header
+	if (_size + _offset <= sqlite_header_size) {
+		std::memcpy(_buffer, _header + _offset, _size);
+
+		return SQLITE_OK;
+	}
+
 	auto _context = *reinterpret_cast<encryption_context**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 2);
 	auto _result = xread_base(_file, _buffer, _size, _offset);
 
-	printf("[%p]: read %i bytes from %lli\n", _file, _size, _offset);
-
-	/*if (_size % 4096) {
-		for (int i = 0; i < _size; ++i) {
-			printf("%02x ", reinterpret_cast<uint8_t*>(_buffer)[i]);
-			if ((i + 1) % 16 == 0) {
-				puts("");
-			}
-		}
-		if (_size % 16) {
-			puts("");
-		}
-	}*/
-
 	// Decrypt
 	if (_context->does_something()) {
-		_context->decrypt(_offset / _size, _buffer, _size, _buffer);
+		_context->decrypt(_offset / _size, _buffer, _size);
 	}
 
 	return _result;
@@ -78,17 +96,30 @@ inline int xwrite_base(sqlite3_file * _file, const void * _buffer, int _size, sq
 
 inline int xwrite_derived(sqlite3_file * _file, const void * _buffer, int _size, sqlite3_int64 _offset)
 {
+#if defined(PRINT_OUTPUT)
+	printf("[%p]: write %i bytes at %lli\n", _file, _size, _offset);
+#endif
+
+	// Write into the SQLite header
+	if (_size + _offset <= sqlite_header_size) {
+		auto _header = *reinterpret_cast<int8_t**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 3);
+
+		std::memcpy(_header + _offset, _buffer, _size);
+		_header[header_control_byte] |= header_modified;
+
+		return SQLITE_OK;
+	}
+
 	auto _context = *reinterpret_cast<encryption_context**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 2);
+	std::unique_ptr<int8_t[]> _encrypted;
 
 	// Encrypt
 	if (_context->does_something()) {
-		std::unique_ptr<int8_t[]> _encrypted(new int8_t[_size]);
-
-		printf("[%p]: write %i bytes at %lli sectorsize: %i\n", _file, _size, _offset, _file->pMethods->xSectorSize(_file));
+		_encrypted.reset(new int8_t[_size]);
 
 		_context->encrypt(_offset / _size, _buffer, _size, _encrypted.get());
 
-		return xwrite_base(_file, _encrypted.get(), _size, _offset + _context->last_encryption_offset());
+		_buffer = _encrypted.get();
 	}
 
 	// Write
@@ -104,15 +135,9 @@ inline int xsync_base(sqlite3_file * _file, int _flags)
 
 inline int xsync_derived(sqlite3_file * _file, int _flags)
 {
-	auto _context = *reinterpret_cast<encryption_context**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 2);
-
+#if defined(PRINT_OUTPUT)
 	printf("[%p]: syncing\n", _file);
-
-	auto _result = _context->sync();
-
-	if (_result != SQLITE_OK) {
-		return _result;
-	}
+#endif
 
 	return xsync_base(_file, _flags);
 }
@@ -159,14 +184,14 @@ inline int xopen_derived(sqlite3_vfs * _vfs, const char * _name, sqlite3_file * 
 		auto _methods = new sqlite3_io_methods(*_file->pMethods);
 		auto _context = reinterpret_cast<encryption_context*>(_addr);
 
-		_context->set_read_callback(std::bind(&xread_base, _file, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
-		_context->set_sector_size(_file->pMethods->xSectorSize(_file));
-
 		// Save context and base methods
+		*reinterpret_cast<int8_t**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 3) = new int8_t[sqlite_header_size + 1]{} + 1;
 		*reinterpret_cast<encryption_context**>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*) * 2) = _context;
 		*reinterpret_cast<base_method_type*>(reinterpret_cast<int8_t*>(_file) + encrypted_vfs.szOsFile - sizeof(void*)) = _file->pMethods;
 
+#if defined(PRINT_OUTPUT)
 		printf("open %s with %s and save to %p\n", _name, _vfs->zName, _file);
+#endif
 
 		_methods->xClose = &xclose_derived;
 		_methods->xRead = &xread_derived;
@@ -175,7 +200,7 @@ inline int xopen_derived(sqlite3_vfs * _vfs, const char * _name, sqlite3_file * 
 
 		_file->pMethods = _methods;
 	} else {
-		std::memset(reinterpret_cast<int8_t*>(_file + encrypted_vfs.szOsFile - sizeof(void*) * 2), 0, sizeof(void*) * 2);
+		std::memset(reinterpret_cast<int8_t*>(_file + encrypted_vfs.szOsFile - sizeof(void*) * 3), 0, sizeof(void*) * 3);
 	}
 
 	return _result;
@@ -191,7 +216,7 @@ void register_encrypted_vfs()
 
 	std::memcpy(&encrypted_vfs, _default, sizeof(encrypted_vfs));
 
-	encrypted_vfs.szOsFile += sizeof(void*) * 2;
+	encrypted_vfs.szOsFile += sizeof(void*) * 3;
 	encrypted_vfs.pNext = nullptr;
 	encrypted_vfs.zName = SQLITE3_ENCRYPTED_VFS_NAME;
 	encrypted_vfs.xOpen = &xopen_derived;

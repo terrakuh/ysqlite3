160995036633a78333f4bdb63ab89249a6d58950
terrakuh <greybley@yahoo.de>
2018-10-30 09:05:56 +0100

    Fix filepath encoding of encrypted_database

--- sqlite3/encrypted_database.cpp
+++ sqlite3/encrypted_database.cpp
@@ -14,11 +14,17 @@ namespace sqlite3
 #include "sqlite3.h"
 
 
-encrypted_database::encrypted_database(encryption_context * _context, const char * _filename, int _mode)
+encrypted_database::encrypted_database(encryption_context * _context, const char * _path, int _mode)
 {
 	std::stringstream _s;
+	const char * _ptr;
 
-	_s << std::hex << reinterpret_cast<intptr_t>(_context) << '-' << _filename;
+	if ((_ptr = std::strrchr(_path, '\\')) || (_ptr = std::strrchr(_path, '/'))) {
+		_s.write(_path, _ptr - _path);
+		_path = _ptr + 1;
+	}
+
+	_s << std::hex << reinterpret_cast<intptr_t>(_context) << '-' << _path;
 
 	if (sqlite3_open_v2(_s.str().c_str(), &_connection->get<sqlite3>(), _mode, SQLITE3_ENCRYPTED_VFS_NAME) != SQLITE_OK) {
 		throw database_error(sqlite3_errmsg(_connection->get<sqlite3>()));
--- sqlite3/encrypted_database.hpp
+++ sqlite3/encrypted_database.hpp
@@ -12,7 +12,7 @@ namespace sqlite3
 class encrypted_database : public database
 {
 public:
-	encrypted_database(encryption_context * _context, const char * _filename, int _mode = SO_READWRITE | SO_CREATE);
+	encrypted_database(encryption_context * _context, const char * _path, int _mode = SO_READWRITE | SO_CREATE);
 
 protected:
 	encryption_context * _context;

178f808f215933f3c6e72f3847892422c367e776
Yunus Ayar <greybley@yahoo.de>
2019-01-22 12:19:44 +0100

    Add scope_exit

---
+++ sqlite3/scope_exit.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <functional>
+
+
+namespace ysqlite3
+{
+
+/**
+ * @brief A simple scope guard.
+*/
+class scope_exit
+{
+public:
+	/** The exit function type. */
+	typedef std::function<void()> function_t;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param [in] _exit The exit function.
+	*/
+	scope_exit(function_t && _exit) noexcept;
+	/**
+	 * Destructor.
+	 *
+	 * @throws See @a _exit.
+	*/
+	~scope_exit();
+
+private:
+	/** The function that is called on destruction. */
+	function_t _exit;
+};
+
+}

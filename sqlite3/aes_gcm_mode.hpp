63eaf14161a209a4082b138b97be0a15b742c163
Yunus Ayar <greybley@yahoo.de>
2019-01-25 21:38:16 +0100

    Add AES-GCM support

---
+++ sqlite3/aes_gcm_mode.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <memory>
+
+#include "encryption_mode.hpp"
+
+
+namespace ysqlite3
+{
+
+class aes_gcm_mode : public encryption_mode
+{
+public:
+	aes_gcm_mode();
+	virtual ~aes_gcm_mode();
+	virtual bool encrypt(const_key_t _key, const_buffer_t _iv, const_buffer_t _aad, size_t _aad_size, const_buffer_t _input, size_t _size, buffer_t _output, data_t _data, size_t _max_data_size) override;
+	virtual bool decrypt(const_key_t _key, const_buffer_t _iv, const_buffer_t _aad, size_t _aad_size, const_buffer_t _input, size_t _size, buffer_t _output, const_data_t _data, size_t _max_data_size) override;
+	virtual int iv_length() const noexcept override;
+	virtual int block_size() const noexcept override;
+
+private:
+	struct impl;
+
+	std::unique_ptr<impl> _impl;
+
+	bool encrypt_decrypt(const_key_t _key, const_buffer_t _iv, const_buffer_t _aad, size_t _aad_size, const_buffer_t _input, size_t _size, buffer_t _output, data_t _data, bool _encrypt);
+};
+
+}

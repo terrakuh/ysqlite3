2a10d5d3f784c189a5a2676ced18a64de1889ca1
Yunus Ayar <greybley@yahoo.de>
2019-06-20 08:28:49 +0200

    Update no_encryption_context.hpp

--- sqlite3/no_encryption_context.hpp
+++ sqlite3/no_encryption_context.hpp
@@ -15,6 +15,18 @@ public:
 	virtual void store_app_data(data_t _data) override
 	{
 	}
+	virtual void apply_key() override
+	{
+	}
+	virtual void revert_key() override
+	{
+	}
+	virtual void set_key(const key_t & _key, bool _encrypt) override
+	{
+	}
+	virtual void set_alogrithm(const char * _algorithm, bool _encrypt) override
+	{
+	}
 	virtual bool encrypt(id_t _id, const_buffer_t _input, size_t _size, buffer_t _output, data_t _data) override
 	{
 		return false;

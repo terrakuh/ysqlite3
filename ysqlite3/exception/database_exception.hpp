f65b037668cc625ba5c91215a5b05d56c572aa62
Yunus Ayar <greybley@yahoo.de>
2019-12-27 01:35:27 +0100

    Update database_exception.hpp

--- ysqlite3/exception/database_exception.hpp
+++ ysqlite3/exception/database_exception.hpp
@@ -6,13 +6,30 @@
 namespace ysqlite3 {
 namespace exception {
 
+/**
+ Thrown if an operation on the database failed. A database error has an additional SQLite3 specific error code.
+*/
 class database_exception : public base_exception
 {
 public:
     database_exception(int error_code, const std::string& msg, const char* file, const char* function, int line)
         : base_exception(msg + " (error_code=" + std::to_string(error_code) + "; " + sqlite3_errstr(error_code), file,
                          function, line)
-    {}
+    {
+        _error_code = error_code;
+    }
+    /**
+     Returns the error code of the exception.
+
+     @returns a SQLite3 specific error code
+    */
+    int error_code() const noexcept
+    {
+        return _error_code;
+    }
+
+private:
+    int _error_code;
 };
 
 } // namespace exception

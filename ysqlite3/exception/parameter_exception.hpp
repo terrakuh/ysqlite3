dcdc6af5ef8e3983ce7c43a438e7a32bc8400833
Yunus Ayar <greybley@yahoo.de>
2019-12-28 13:14:15 +0100

    Create parameter_exception.hpp

---
+++ ysqlite3/exception/parameter_exception.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "base_exception.hpp"
+
+namespace ysqlite3 {
+namespace exception {
+
+class parameter_exception : public base_exception
+{
+public:
+    using base_exception::base_exception;
+};
+
+} // namespace exception
+} // namespace ysqlite3

5a25d38ae7dc24eade3df51bc9456efc043ea76e
Yunus Ayar <greybley@yahoo.de>
2019-12-29 01:56:14 +0100

    Update statement.hpp

--- ysqlite3/statement.hpp
+++ ysqlite3/statement.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#include "exception/closed_statement_exception.hpp"
 #include "exception/database_exception.hpp"
 #include "exception/parameter_exception.hpp"
 #include "scope_exit.hpp"
 #include "sqlite3.h"
 
 #include <cstddef>
+#include <gsl/gsl>
 #include <string>
 #include <utility>
 
@@ -15,249 +15,280 @@ namespace ysqlite3 {
 class statement
 {
 public:
-    class index
-    {
-    public:
-        index(int index) noexcept
-        {
-            _index = index;
-            _name  = nullptr;
-        }
-        index(const std::string& name) noexcept
-        {
-            _index = 0;
-            _name  = name.c_str();
-        }
-        index(const char* name) noexcept
-        {
-            _index = 0;
-        }
+	class index
+	{
+	public:
+		index(int index)
+		{
+			Expects(index > 0);
 
-    private:
-        friend statement;
+			_index = index;
+			_name  = nullptr;
+		}
+		index(const std::string& name)
+		{
+			Expects(not name.empty());
 
-        int _index;
-        const char* _name;
-    };
+			_index = 0;
+			_name  = name.c_str();
+		}
+		index(gsl::not_null<const char*> name)
+		{
+			Expects(name.get()[0] != 0);
 
-    /**
-     Associates this object with an SQLite statement.
+			_index = 0;
+			_name  = name;
+		}
 
-     @param[in] stmt the statement
-    */
-    statement(sqlite3_stmt* stmt) noexcept
-    {
-        _statement = stmt;
-    }
-    statement(const statement& copy) = delete;
-    /**
-     Move-Constructor. After this operation `move` is guaranteed to be closed.
+	private:
+		friend statement;
 
-     @param[in,out] move the other statement
-    */
-    statement(statement&& move) noexcept
-    {
-        _statement      = move._statement;
-        move._statement = nullptr;
-    }
-    /**
-     Destructor. Closes the statement.
+		int _index;
+		const char* _name;
+	};
 
-     @see statement::close()
-    */
-    virtual ~statement()
-    {
-        try {
-            close();
-        } catch (...) {
-        }
-    }
-    /**
-     Clears all set bindings.
+	/**
+	 Associates this object with an SQLite statement.
 
-     @throws exception::closed_statement_exception if the statement is closed
-    */
-    void clear_bindings()
-    {
-        _assert_statement();
+	 @param[in] stmt the statement
+	*/
+	statement(sqlite3_stmt* stmt) noexcept
+	{
+		_statement = stmt;
+	}
+	statement(const statement& copy) = delete;
+	/**
+	 Move-Constructor. After this operation `move` is guaranteed to be closed.
 
-        sqlite3_clear_bindings(_statement);
-    }
-    /**
-     Evaluates the SQL statement. The returned data is discarded.
+	 @param[in,out] move the other statement
+	*/
+	statement(statement&& move) noexcept
+	{
+		_statement      = move._statement;
+		move._statement = nullptr;
+	}
+	/**
+	 Destructor. Closes the statement.
 
-     @throws see step()
-    */
-    void finish()
+	 @see statement::close()
+	*/
+	virtual ~statement()
+	{
+		try {
+			close();
+		} catch (...) {
+		}
+	}
+	/**
+	 Clears all set bindings.
 
-    {
-        while (step())
-            ;
-    }
-    /**
-     Closes the statement. Closing a closed statement has no effect.
+	 @pre the statement is not closed
+	*/
+	void clear_bindings()
+	{
+		Expects(not closed());
 
-     @throws exception::database_exception if closing failed for some reason
-    */
-    void close()
-    {
-        auto error = sqlite3_finalize(_statement);
+		sqlite3_clear_bindings(_statement);
+	}
+	/**
+	 Evaluates the SQL statement. The returned data is discarded.
 
-        _statement = nullptr;
+	 @pre the statement is not closed
+	 @post the statement is reset
 
-        if (error != SQLITE_OK) {
-            YSQLITE_THROW(exception::database_exception, error, "failed to close the statement");
-        }
-    }
-    /**
-     Returns whether the statement is closed or not.
+	 @throws exception::database_exception if the step could not be executed properly
+	*/
+	void finish()
+	{
+		Expects(not closed());
 
-     @returns `true` if the statement is closed, otherwise `false`
-    */
-    bool closed() noexcept
-    {
-        return _statement == nullptr;
-    }
-    /**
-     Evaluates the SQL statement step by step and returns the data. This function must be called one or more times.
+		// step until SQLITE_DONE is returned
+		while (true) {
+			auto error = sqlite3_step(_statement);
 
-     @returns `true` if a row was fetched or `false` if no more rows are available, i.e. the statement is finished
-     @throws exception::closed_statement_exception if the statement is closed
-     @throws exception::database_exception if the step could not be executed properly
-    */
-    bool step()
-    {
-        _assert_statement();
+			if (error != SQLITE_ROW) {
+				auto resetter = at_scope_exit([this] { sqlite3_reset(_statement); });
 
-        auto error = sqlite3_step(_statement);
+				if (error == SQLITE_DONE) {
+					return;
+				}
 
-        if (error == SQLITE_ROW) {
-            return true;
-        }
+				YSQLITE_THROW(exception::database_exception, error, "failed to step");
+			}
+		}
+	}
+	/**
+	 Closes the statement. Closing a closed statement has no effect.
 
-        auto resetter = at_scope_exit([this] { sqlite3_reset(_statement); });
+	 @post the database is closed
 
-        if (error == SQLITE_DONE) {
-            return false;
-        }
+	 @throws exception::database_exception if closing failed for some reason
+	*/
+	void close()
+	{
+		auto error = sqlite3_finalize(_statement);
 
-        YSQLITE_THROW(exception::database_exception, error, "failed to step");
-    }
-    statement& bind(const index& index, std::nullptr_t)
-    {
-        return _bind(index, sqlite3_bind_null);
-    }
-    statement& bind(const index& index, double _value)
-    {
-        return _bind(index, sqlite3_bind_double, _value);
-    }
-    statement& bind(const index& index, int _value)
-    {
-        return _bind(index, sqlite3_bind_int, _value);
-    }
-    statement& bind64(const index& index, sqlite3_int64 _value)
-    {
-        return _bind(index, sqlite3_bind_int64, _value);
-    }
-    statement& bind_zeros(const index& index, int _size)
-    {
-        return _bind(index, sqlite3_bind_zeroblob, _size);
-    }
-    statement& bind_zeros64(const index& index, sqlite3_uint64 _size)
-    {
-        return _bind(index, sqlite3_bind_zeroblob64, _size);
-    }
-    /**
-     Returns whether the statement makes no direct changes to the database.
+		_statement = nullptr;
 
-     @returns `true` if read-only, otherwise `false`
-     @throws exception::closed_statement_exception if the statement is closed
-    */
-    bool readonly()
-    {
-        _assert_statement();
+		if (error != SQLITE_OK) {
+			YSQLITE_THROW(exception::database_exception, error, "failed to close the statement");
+		}
+	}
+	/**
+	 Returns whether the statement is closed or not.
 
-        return sqlite3_stmt_readonly(_statement);
-    }
-    /**
-     Returns the parameter count that can be bound.
+	 @returns `true` if the statement is closed, otherwise `false`
+	*/
+	bool closed() noexcept
+	{
+		return _statement == nullptr;
+	}
+	/**
+	 Evaluates the SQL statement step by step and returns the data. This function must be called one or more times.
 
-     @returns the parameter count
-     @throws exception::closed_statement_exception if the statement is closed
-    */
-    int parameter_count()
-    {
-        _assert_statement();
+	 @pre the statement is not closed
 
-        return sqlite3_bind_parameter_count(_statement);
-    }
-    /**
-     Returns the column count of the result rows.
+	 @returns `true` if a row was fetched or `false` if no more rows are available, i.e. the statement is finished
+	 @throws exception::database_exception if the step could not be executed properly
+	*/
+	bool step()
+	{
+		Expects(not closed());
 
-     @returns the column count
-     @throws exception::closed_statement_exception if the statement is closed
-    */
-    int column_count()
-    {
-        _assert_statement();
-    
-        return sqlite3_column_count(_statement);
-    }
-    /**
-     Returns the SQLite3 statment handle.
+		auto error = sqlite3_step(_statement);
 
-     @returns the statment handle or `nullptr` if the statment is closed
-    */
-    sqlite3_stmt* handle() noexcept
-    {
-        return _statement;
-    }
-    /**
-     Returns the SQLite3 statment handle.
+		if (error == SQLITE_ROW) {
+			return true;
+		}
 
-     @returns the statment handle or `nullptr` if the statment is closed
-    */
-    const sqlite3_stmt* handle() const noexcept
-    {
-        return _statement;
-    }
+		auto resetter = at_scope_exit([this] { sqlite3_reset(_statement); });
+
+		if (error == SQLITE_DONE) {
+			return false;
+		}
+
+		YSQLITE_THROW(exception::database_exception, error, "failed to step");
+	}
+	statement& bind(const index& index, std::nullptr_t)
+	{
+		return _bind(index, sqlite3_bind_null);
+	}
+	statement& bind(const index& index, double _value)
+	{
+		return _bind(index, sqlite3_bind_double, _value);
+	}
+	statement& bind(const index& index, int _value)
+	{
+		return _bind(index, sqlite3_bind_int, _value);
+	}
+	statement& bind64(const index& index, sqlite3_int64 _value)
+	{
+		return _bind(index, sqlite3_bind_int64, _value);
+	}
+	statement& bind_zeros(const index& index, int _size)
+	{
+		return _bind(index, sqlite3_bind_zeroblob, _size);
+	}
+	statement& bind_zeros64(const index& index, sqlite3_uint64 _size)
+	{
+		return _bind(index, sqlite3_bind_zeroblob64, _size);
+	}
+	/**
+	 Returns whether the statement makes no direct changes to the database.
+
+	 @pre the statement is not closed
+
+	 @returns `true` if read-only, otherwise `false`
+	*/
+	bool readonly()
+	{
+		Expects(not closed());
+
+		return sqlite3_stmt_readonly(_statement);
+	}
+	/**
+	 Returns the parameter count that can be bound.
+
+	 @pre the statement is not closed
+
+	 @returns the parameter count
+	*/
+	int parameter_count()
+	{
+		Expects(not closed());
+
+		return sqlite3_bind_parameter_count(_statement);
+	}
+	/**
+	 Returns the column count of the result rows.
+
+	 @pre the statement is not closed
+
+	 @returns the column count
+	*/
+	int column_count()
+	{
+		Expects(not closed());
+
+		return sqlite3_column_count(_statement);
+	}
+	/**
+	 Returns the SQLite3 statment handle.
+
+	 @returns the statment handle or `nullptr` if the statment is closed
+	*/
+	sqlite3_stmt* handle() noexcept
+	{
+		return _statement;
+	}
+	/**
+	 Returns the SQLite3 statment handle.
+
+	 @returns the statment handle or `nullptr` if the statment is closed
+	*/
+	const sqlite3_stmt* handle() const noexcept
+	{
+		return _statement;
+	}
+	gsl::owner<sqlite3_stmt*> release() noexcept
+	{
+		auto p = _statement;
+
+		_statement = nullptr;
+
+		return p;
+	}
 
 private:
-    sqlite3_stmt* _statement;
+	sqlite3_stmt* _statement;
 
-    void _assert_statement()
-    {
-        if (closed()) {
-            YSQLITE_THROW(exception::closed_statement_exception, "the statement is closed");
-        }
-    }
-    int _to_column_index(const index& index)
-    {
-        if (index._name) {
-            auto i = sqlite3_bind_parameter_index(_statement, index._name);
+	int _to_column_index(const index& index)
+	{
+		if (index._name) {
+			auto i = sqlite3_bind_parameter_index(_statement, index._name);
 
-            if (!i) {
-                YSQLITE_THROW(exception::parameter_exception, "unkown parameter name");
-            }
+			if (!i) {
+				YSQLITE_THROW(exception::parameter_exception, "unkown parameter name");
+			}
 
-            return i;
-        }
+			return i;
+		}
 
-        return index._index;
-    }
-    template<typename Binder, typename... Args>
-    statement& _bind(const index& index, Binder&& binder, Args&&... args)
-    {
-        _assert_statement();
+		return index._index;
+	}
+	template<typename Binder, typename... Args>
+	statement& _bind(const index& index, Binder&& binder, Args&&... args)
+	{
+		Expects(not closed());
 
-        auto error = binder(_statement, _to_column_index(index), std::forward<Args>(args)...);
+		auto error = binder(_statement, _to_column_index(index), std::forward<Args>(args)...);
 
-        if (error != SQLITE_OK) {
-            YSQLITE_THROW(exception::database_exception, error, "could not bind value");
-        }
+		if (error != SQLITE_OK) {
+			YSQLITE_THROW(exception::database_exception, error, "could not bind value");
+		}
 
-        return *this;
-    }
+		return *this;
+	}
 };
 
 } // namespace ysqlite3

87139504c010a9c0d7c36d4f372b034725ff9be3
Yunus Ayar <greybley@yahoo.de>
2020-03-17 22:48:02 +0100

    Update regexp

---
+++ src/function/regexp.cpp
@@ -0,0 +1,37 @@
+#include "function/regexp.hpp"
+
+using namespace ysqlite3::function;
+
+regexp::regexp(bool cache) noexcept
+    : function(2, true, false, text_encoding::utf8), _cache(cache ? new std::map<std::string, std::regex>() : nullptr)
+{}
+
+void regexp::run(sqlite3_context* context, int argc, sqlite3_value** argv)
+{
+	std::regex p;
+	std::regex* pattern = nullptr;
+
+	// cached
+	if (_cache) {
+		std::string str(reinterpret_cast<const char*>(sqlite3_value_text(argv[0])));
+		auto r = _cache->find(str);
+
+		if (r != _cache->end()) {
+			pattern = &r->second;
+		} else {
+			p       = std::regex(str, std::regex_constants::ECMAScript | std::regex_constants::optimize);
+			pattern = &_cache->emplace(std::move(str), std::move(p)).first->second;
+		}
+	} else {
+		p       = std::regex(reinterpret_cast<const char*>(sqlite3_value_text(argv[0])));
+		pattern = &p;
+	}
+
+	std::cmatch match;
+
+	if (std::regex_match(reinterpret_cast<const char*>(sqlite3_value_text(argv[1])), match, *pattern)) {
+		sqlite3_result_int(context, 1);
+	} else {
+		sqlite3_result_int(context, 0);
+	}
+}
--- ysqlite3/function/regexp.hpp
+++ ysqlite3/function/regexp.hpp
@@ -14,41 +14,10 @@ namespace function {
 class regexp : public function
 {
 public:
-	regexp(bool cache = true) noexcept
-	    : function(2, true, false, text_encoding::utf8),
-	      _cache(cache ? new std::map<std::string, std::regex>() : nullptr)
-	{}
+	regexp(bool cache = true) noexcept;
 
 protected:
-	virtual void run(sqlite3_context* context, int argc, sqlite3_value** argv) override
-	{
-		std::regex p;
-		std::regex* pattern = nullptr;
-
-		// cached
-		if (_cache) {
-			std::string str(reinterpret_cast<const char*>(sqlite3_value_text(argv[0])));
-			auto r = _cache->find(str);
-
-			if (r != _cache->end()) {
-				pattern = &r->second;
-			} else {
-				p       = std::regex(str, std::regex_constants::ECMAScript | std::regex_constants::optimize);
-				pattern = &_cache->emplace(std::move(str), std::move(p)).first->second;
-			}
-		} else {
-			p       = std::regex(reinterpret_cast<const char*>(sqlite3_value_text(argv[0])));
-			pattern = &p;
-		}
-
-		std::cmatch match;
-
-		if (std::regex_match(reinterpret_cast<const char*>(sqlite3_value_text(argv[1])), match, *pattern)) {
-			sqlite3_result_int(context, 1);
-		} else {
-			sqlite3_result_int(context, 0);
-		}
-	}
+	void run(sqlite3_context* context, int argc, sqlite3_value** argv) override;
 
 private:
 	std::unique_ptr<std::map<std::string, std::regex>> _cache;
